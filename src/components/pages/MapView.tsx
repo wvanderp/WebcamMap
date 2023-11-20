6e4ad70ab537ede37b4db7010cc8e98c56a7e805
Malte <w-m-b-r@t-online.de>
2023-11-20 03:34:39 +0100

    Linter says we need a semicolon here

--- src/components/pages/MapView.tsx
+++ src/components/pages/MapView.tsx
@@ -15,7 +15,7 @@ function MapView() {
         if (webcam === null) {
             return null;
         }
-        const rotationAngle = webcam.direction === undefined ? 0 : webcam.direction - 90
+        const rotationAngle = webcam.direction === undefined ? 0 : webcam.direction - 90;
         return (
             <Marker key={webcam.osmID} position={[webcam.lat, webcam.lon]} icon={MarkerIcon} rotationAngle={rotationAngle} rotationOrigin={'center center'}>
                 <Popup>
